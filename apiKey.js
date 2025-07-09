0b30287ebb5eec8e739a9091eba76258929166a1
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2025-07-09 15:52:49 -0700

    add box to apiKey.js

--- apiKey.js
+++ apiKey.js
@@ -1,4 +1,5 @@
 /* Copy this into a file named apiKey.js */
 const apiKey = '1DpF4lnxxGr0IjKbKlOm58A9GuKbFyKaxh2PDK87x';
 const appId = 'ai.rhq.factory';
-export default {apiKey, appId};
+const box = "https://rhq.ai";
+export default {apiKey, appId, box};
